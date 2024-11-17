529934e5032a57982139719c174f42b4f0dee7f5
Harsh Vishwakarma <Harsh.Vishwakarma@gds.ey.com>
2024-11-17 18:32:29 +0530

    initial commit and header done

---
+++ app/(root)/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  // console.log('home');
+
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">Pitch your startup, <br />Connect with Entrepreneurs</h1>
+        <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competition</p>
+      </section>
+    </>
+  );
+}
